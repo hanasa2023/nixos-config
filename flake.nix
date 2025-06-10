1e5a746121442ceb3a09d2c97d7a1e662a7ac030
hanasaki <hanasakayui2022@gmail.com>
2025-06-10 21:13:09 +0800

    🐛 Fix hm backup with 25.11

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = inputs // specialArgs;
-            home-manager.backupFileExtension = "backup";
+            home-manager.backupFileExtension = "hm-backup";
             home-manager.users.${username} =
               import ./users/${username}/home.nix;
           }
